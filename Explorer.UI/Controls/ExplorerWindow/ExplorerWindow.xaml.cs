6156f8b69c4810357f9f6c38d38a7cc5839eaf55
Nicolos  Hideki <nikita.terebetsky@gmail.com>
2026-07-27 14:36:56 +0300

    Theme Fixes, Doubleclick Fixes

--- Explorer.UI/Controls/ExplorerWindow/ExplorerWindow.xaml.cs
+++ Explorer.UI/Controls/ExplorerWindow/ExplorerWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Explorer.UI.Controls;
 
@@ -7,5 +9,16 @@ public partial class ExplorerWindow : UserControl
     public ExplorerWindow()
     {
         InitializeComponent();
+    }
+
+    private void FileListItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        if (sender is not FrameworkElement { DataContext: { } item })
+            return;
+
+        var property = item.GetType().GetProperty("OpenCommand");
+
+        if (property?.GetValue(item) is ICommand command && command.CanExecute(null))
+            command.Execute(null);
     }
 }
